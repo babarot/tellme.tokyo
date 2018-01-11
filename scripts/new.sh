2504131e940d281e931858f869851195dce68819
b4b4r07 <b4b4r07@gmail.com>
2018-01-11 21:42:21 +0900

    Update "new" script

--- scripts/new.sh
+++ scripts/new.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-title="${1:?}"
+title="${1}"
+if [[ -z $title ]]; then
+    echo "No title is given, so open the existing posts..."
+    title="$(ls -1 content/post | fzf --height 40% --reverse)"
+    title="${title##*/}"
+fi
+if [[ -f content/post/${title%.md}.md ]]; then
+    $EDITOR content/post/${title%.md}.md
+    exit $?
+fi
 hugo new post/${title%.md}.md --editor=$EDITOR
