f64bad72e1d34066f4424076f4951f54783985d3
b4b4r07 <b4b4r07@gmail.com>
2018-10-10 00:33:58 +0900

    Move some functions

--- cmd/blog/article.go
+++ cmd/blog/article.go
@@ -127,16 +127,3 @@ func readFile(path string) ([]byte, error) {
 	}
 	return []byte(content), scanner.Err()
 }
-
-func uniqSlice(s []string) []string {
-	for i := 0; i < len(s); i++ {
-		for i2 := i + 1; i2 < len(s); i2++ {
-			if s[i] == s[i2] {
-				// delete
-				s = append(s[:i2], s[i2+1:]...)
-				i2--
-			}
-		}
-	}
-	return s
-}
