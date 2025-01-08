146df586dbbb0bc565e5fb5c84885df82bceec5d
babarot <b4b4r07@gmail.com>
2025-01-09 01:29:30 +0900

    Tweak scripts

--- scripts/blog.sh
+++ scripts/blog.sh
@@ -1,10 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # requires:
 # - bash 4.4+
 # - jq
 # - yq
 # - fzf
+# - tac
+# - fd (sharkdp/fd)
 # - bat (sharkdp/bat)
 # - mmv (itchyny/mmv)
 # - nvim
@@ -23,19 +25,40 @@ EOF
 main() {
   local json
   local actions=(edit create)
+  local no_draft=false
+  local -a args
+
+  while (( ${#} > 0 ))
+  do
+    case "${1}" in
+      --no-draft)
+        no_draft=true
+        ;;
+      -*)
+        echo "${1}: no such option" >&2
+        return 1
+        ;;
+      *)
+        args+=("${1}")
+        ;;
+    esac
+    shift
+  done
+
   while true
   do
-    action=$(printf "%s\n" "${actions[@]}" | command fzf --header 'Any action? Press CTRL-C to quit')
+    action=$(printf "%s\n" "${actions[@]}" | fzf --header 'Any action? Press CTRL-C to quit')
     case "${action}" in
       create)
         create
         ;;
       edit)
-        if [[ -z ${json} ]]; then
+        if [[ -z ${json} ]] && ${no_draft}; then
           json="$(
           for file in $(fd -tf '\.md$' content/post)
           do
-            yq -o json --no-colors --front-matter="extract" "${file}" | jq -c ". + {\"file\": \"${file}\"}"
+            yq -o json --no-colors --front-matter="extract" "${file}" |
+              jq -c ". + {\"file\": \"${file}\"}"
           done | jq --slurp
           )"
         fi
@@ -68,7 +91,11 @@ edit() {
   do
     # mapfile requires bash 4.4+
     mapfile -t files < <(
-    echo "${json}" | jq -r 'reverse | .[] | select(.draft | not) | .file' |
+    if [[ -n ${json} ]]; then
+      echo "${json}" | jq -r 'reverse | .[] | select(.draft | not) | .file'
+    else
+      fd -tf '\.md$' content/post | tac
+    fi |
       fzf \
       --header 'Press CTRL-R to reveal in Finder, CTRL-V to move to...' \
       --preview 'bat --language=markdown --color=always --style=numbers {}' \
